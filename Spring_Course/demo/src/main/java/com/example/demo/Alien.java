e60f13a989f48c43f5df9e9bf210bc45338ac0aa
sarowargupta <sarowargupta2001@gmail.com>
2025-03-22 00:30:31 +0530

    Autowired in spring boot

--- Spring_Course/demo/src/main/java/com/example/demo/Alien.java
+++ Spring_Course/demo/src/main/java/com/example/demo/Alien.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien
 {
+
+    //dependency injection
+//    public void code()
+//    {
+//        System.out.println("Im Coding");
+//    }
+
+
+
+    //using the annotation autowired
+
+    @Autowired
+    Laptop lap;
     public void code()
     {
-        System.out.println("Im Coding");
+        lap.compile();
     }
+
 }
 
