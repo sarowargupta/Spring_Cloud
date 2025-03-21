a3f663b3595b548b1e9965f6f7f7ae17d23e91ca
sarowargupta <sarowargupta2001@gmail.com>
2025-03-21 14:23:34 +0530

    Dependency Injection in Spring project

---
+++ Spring_Course/demo/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien
+{
+    public void code()
+    {
+        System.out.println("Im Coding");
+    }
+}
+
